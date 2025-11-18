3d06321b54fc2b201642b6d2cd2511988c5045c2
Abdullah Al Jubair <jubair2810@gmail.com>
2025-11-18 23:13:23 +0600

    refactore config

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -10,17 +10,11 @@ export default {
     process.env.NODE_MODULE === "production"
       ? process.env.DATABASE_URL_PROD
       : process.env.DATABASE_URL,
-  redis_api:
-    process.env.NODE_MODULE === "production"
-      ? process.env.REDIS_URL
-      : process.env.REDIS_URL_LOCAL,
-  redis_password: process.env.REDIS_PASSWORD,
+
   fronted_url:
     process.env.NODE_MODULE === "development"
       ? process.env.FRONTEND_URL_DEV
       : process.env.FRONTEND_URL_PROD,
-  mail_user: process.env.MAIL_USER,
-  mail_secret: process.env.MAIL_SECRET,
 
   jwt: {
     access_secret: process.env.ACCESS_TOKEN,
@@ -29,24 +23,8 @@ export default {
     expire_in_back: process.env.EXPIRES_IN_BACK,
     refres_expire_in: process.env.REFRESH_EXPIRES_IN,
   },
-  sms: {
-    api_key: process.env.SMS_APIKEY,
-    sender_id: process.env.SMS_SENDERID,
-    api_url: process.env.SMS_APIURL,
-  },
+
   backend_base_url: process.env.BACKEND_URL,
-  frontend_url:
-    process.env.NODE_ENV === "development"
-      ? process.env.FRONTEDN_URL_DEV
-      : process.env.FRONTEDN_URL_PROD,
+
   node_env: process.env.NODE_ENV,
-  aws: {
-    s3: {
-      accessKeyId: process.env.S3_ACCESS_KEY,
-      secretAccessKey: process.env.S3_SECRET_KEY,
-      region: process.env.S3_BUCKET_REGION,
-      bucketName: process.env.BUCKET_NAME,
-      maxUploadSize: process.env.MAX_FILE_SIZE,
-    },
-  },
 };
